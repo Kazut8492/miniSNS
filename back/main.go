e01b2900903a908ee653bdbf272957a99fdf20b1
Kazuki Taira <kazuki@lift99.co>
2023-02-06 15:45:56 +0200

    added go mod

--- back/main.go
+++ back/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("Hello")
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+	r.Run()
 }
