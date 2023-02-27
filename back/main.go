64bed04dcdb77eee91fe0611ee515dfda8f39c0d
Kazuki Taira <kazuki@lift99.co>
2023-02-27 16:28:53 +0200

    tried gorm

--- back/main.go
+++ back/main.go
@@ -1,12 +1,63 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
 	"time"
 )
 
+type User struct {
+	gorm.Model
+
+	ID        int    `json:"id"`
+	Username  string `json:"username"`
+	Password  string `json:"password"`
+	Email     string `json:"email"`
+	Age       int    `json:"age"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+type Post struct {
+	gorm.Model
+
+	ID      int    `json:"id"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+	Author  string `json:"author"`
+}
+
+func dbInit() *gorm.DB {
+	dsn := "root:pass1111@tcp(127.0.0.1:3306)/toit?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	return db
+}
+
+func insert(db *gorm.DB) {
+	user := User{
+		Username: "太郎",
+		Age:      20,
+	}
+	result := db.Create(&user)
+	if result.Error != nil {
+		log.Fatal(result.Error)
+	}
+	fmt.Println("count:", result.RowsAffected)
+}
+
 func main() {
+
+	db := dbInit()
+	db.AutoMigrate(&User{})
+	insert(db)
+
 	router := gin.Default()
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:3000"},
@@ -17,7 +68,7 @@ func main() {
 		MaxAge:           12 * time.Hour,
 	}))
 
-	router.GET("/", func(c *gin.Context) {
+	router.GET("/home", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"count": 100,
 		})
