4cac47e5867d978f32719602cabf88745bec7fd3
Kazuki Taira <kazuki@lift99.co>
2023-04-05 15:30:37 +0300

    typescript

--- front/src/components/Home.tsx
+++ front/src/components/Home.tsx
@@ -1,26 +1,44 @@
 import React from "react";
 import {useEffect, useState} from "react";
+import {Breadcrumb, Layout, Menu, theme} from "antd";
+
+const {Header, Content, Sider} = Layout;
 
 export const Home: React.FC<{  }> = props => {
     const [data, setData] =  useState<any[]>([])
 
     useEffect(() => {
-        fetch("http://localhost:8080/users")
+        fetch("http://localhost:8080/posts")
             .then(response => response.json())
             .then(data => {
                 console.log(data)
-                setData(data.users);
+                setData(data);
             }
         );
-        console.log("Test")
     }, []);
 
     return (
-        <div>
-            <h1>Home</h1>
-            {data && data.map((user: any) => {
-                return <p>{user.username}</p>
-            })}
-        </div>
+        <Layout>
+            <Header>
+                <div className="logo" />
+                <Menu />
+            </Header>
+            <Layout>
+                <Sider>
+                    <Menu />
+                </Sider>
+                <Layout>
+                    <Breadcrumb />
+                    <Content>Content</Content>
+                </Layout>
+            </Layout>
+        </Layout>
+        // <div>
+        //     <h1>Home</h1>
+        //     {data && data.map((post: any) => {
+        //         return <p>{post.content}</p>
+        //     })}
+        //
+        // </div>
     );
 };
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -13,7 +13,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
         <Routes>
-            <Route path={`/users`} element={<Home />} />
+            <Route path={`/posts`} element={<Home />} />
         </Routes>
     </BrowserRouter>
   </React.StrictMode>
